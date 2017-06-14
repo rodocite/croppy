ee778bd55a133a43bc94baebddb81cd985624e62
Rodolfo Yabut <rodolfoyabut@gmail.com>
2017-06-14 11:48:00 -0700

    Include arc dependency for image uploads

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Croppy.Mixfile do
   def application do
     [mod: {Croppy, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :arc_ecto]]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,14 +30,18 @@ defmodule Croppy.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
+    [
+     {:phoenix, "~> 1.2.1"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:arc, "~> 0.8.0"},
+     {:arc_ecto, "~> 0.7.0"}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
